43658f0d90c370399a689c6682156d28d780e0c4
Marcos Alves <mrcaurelio95@gmail.com>
2025-01-02 15:41:07 -0300

    feat: adicionar funcionalidade para limpar filtros

--- project-rickandmorty/src/components/filterOptions/FilterOptions.jsx
+++ project-rickandmorty/src/components/filterOptions/FilterOptions.jsx
@@ -1,19 +1,23 @@
 import React, { useState } from 'react'
-
+import styles from './FilterOptions.module.scss'
 const FilterOptions = ({ setStatus, setSpecies, setGender }) => {
-
+    const handleClick = () => {
+        setStatus('');
+        setSpecies('');
+        setGender('');
+    }
     return (
         <>
             <form className='d-flex justify-content-center gap-4 mb-4'>
-                <select className='border ' onChange={(e) => setStatus(e.target.value)}>
-                    <option value="">Selecione o status</option>
+                <select className={styles.opt} onChange={(e) => setStatus(e.target.value)}>
+                    <option value="" disabled selected>Selecione o status</option>
                     <option value="alive">Vivo</option>
                     <option value="dead">Morto</option>
                     <option value="unknown">Desconhecido</option>
                     <option value="">Todos</option>
                 </select>
-                <select onChange={(e) => setSpecies(e.target.value)}>
-                    <option value="">Selecione a espécie</option>
+                <select className={styles.opt} onChange={(e) => setSpecies(e.target.value)}>
+                    <option value="" disabled selected>Selecione a espécie</option>
                     <option value="Human">Humano</option>
                     <option value="Alien">Alienígena </option>
                     <option value="Humanoid">Humanoide</option>
@@ -27,14 +31,17 @@ const FilterOptions = ({ setStatus, setSpecies, setGender }) => {
                     <option value="Planet">Planeta</option>
                     <option value="">Todos</option>
                 </select>
-                <select onChange={(e) => setGender(e.target.value)}>
-                    <option value="">Selecione o genero</option>
+                <select className={styles.opt} onChange={(e) => setGender(e.target.value)}>
+                    <option value="" disabled selected>Selecione o genero</option>
                     <option value="male">Macho</option>
                     <option value="female">Fêmea</option>
                     <option value="unknown">Desconhecido</option>
                     <option value="">Todos</option>
                 </select>
+
             </form>
+            <p className={`d-flex justify-content-center gap-4 mb-4 ${styles.btn}`} onClick={handleClick}>Limpar filtro</p>
+
 
         </>
 
