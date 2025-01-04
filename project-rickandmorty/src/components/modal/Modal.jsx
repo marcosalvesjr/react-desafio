c242fd08b8e291183359d5d3b9adde1d89ca5541
Marcos Alves <mrcaurelio95@gmail.com>
2025-01-04 20:14:18 -0300

    feat(modal): atualizar itens exibidos no modal
    - Atualiza o conteúdo e a exibição dos itens no modal.

--- project-rickandmorty/src/components/modal/Modal.jsx
+++ project-rickandmorty/src/components/modal/Modal.jsx
@@ -6,9 +6,32 @@ const Modal = ({ selectedCharacter, open }) => {
       <div>
         <div>
           <h2>{selectedCharacter.name}</h2>
-          <p><strong>Status:</strong> {selectedCharacter.status}</p>
-          <p><strong>Espécie:</strong> {selectedCharacter.species}</p>
-          <p><strong>Gênero:</strong> {selectedCharacter.gender}</p>
+          <img src={selectedCharacter.image} alt="" />
+          <p><strong>Status:</strong>  {selectedCharacter?.status === 'Alive'
+            ? 'Vivo'
+            : selectedCharacter?.status === 'Dead'
+              ? 'Morto'
+              : selectedCharacter?.status === 'unknown'
+                ? 'Desconhecido'
+                : 'Status inválido'}</p>
+          <p><strong>Espécie:</strong> {selectedCharacter?.species === 'Human' ? 'Humano'
+            : selectedCharacter?.species === 'Alien' ? 'Alienígena'
+              : selectedCharacter?.species === 'Humanoid' ? 'Humanoide'
+                : selectedCharacter?.species === 'Poopybutthole' ? 'Poopybutthole'
+                  : selectedCharacter?.species === 'Mythological' ? 'Mitologico'
+                    : selectedCharacter?.species === 'Unknown' ? 'Desconhecido'
+                      : selectedCharacter?.species === 'Animal' ? 'Animal'
+                        : selectedCharacter?.species === 'Disease' ? 'Doença'
+                          : selectedCharacter?.species === 'Robot' ? 'Robo'
+                            : selectedCharacter?.species === 'Cronenberg' ? 'Cronenberg'
+                              : selectedCharacter?.species === 'Planet' ? 'Planeta'
+                                : 'Status inválido'}</p>
+          <p><strong>Gênero:</strong> {selectedCharacter?.gender === 'Male' ? 'Macho'
+            : selectedCharacter?.gender === 'Female' ? 'Fêmea'
+              : selectedCharacter?.gender === 'Unknown' ? 'Desconhecido'
+                : 'Genero inválido'}</p>
+          <p><strong>Origem: </strong> {selectedCharacter.origin?.name === 'unknown' ? 'Desconhecido'
+            : selectedCharacter.origin.name}</p>
           <p><strong>Última localização:</strong> {selectedCharacter.location.name}</p>
 
         </div>
