e75ad4d2618864bd94f06f9e6ad6271959780bdd
Marcos Alves <mrcaurelio95@gmail.com>
2025-01-16 15:30:56 -0300

    refactor(modal): adicionar gênero inválido

--- project-rickandmorty/src/components/modal/Modal.jsx
+++ project-rickandmorty/src/components/modal/Modal.jsx
@@ -39,7 +39,7 @@ const Modal = ({ selectedCharacter, open, setOpen }) => {
             <h2 className='pt-4 pb-2 fw-bolder'>{selectedCharacter.name}</h2>
             <p><strong>Status:</strong>  {statusMap[selectedCharacter?.status] || 'Status inválido.'}</p>
             <p><strong>Espécie:</strong> {speciesMap[selectedCharacter?.species] || 'Espécie inválida.'}</p>
-            <p><strong>Gênero:</strong> {genderMap[selectedCharacter?.gender]}</p>
+            <p><strong>Gênero:</strong> {genderMap[selectedCharacter?.gender] || 'Gênero inválido.'}</p>
             <p><strong>Origem: </strong> {selectedCharacter.origin?.name === 'unknown' ? 'Desconhecido'
               : selectedCharacter.origin.name}</p>
             <p><strong>Última localização:</strong> {selectedCharacter.location.name}</p></div>
