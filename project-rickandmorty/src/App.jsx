1c5bf0a2d6491248033b9b4edd2809273480c8ed
Marcos Alves <mrcaurelio95@gmail.com>
2025-01-23 22:10:16 -0300

    feat: adiciona suporte a query string para sincronizar filtros com a URL

--- project-rickandmorty/src/App.jsx
+++ project-rickandmorty/src/App.jsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
 import Characters from "./components/characters/Characters"
 import Header from "./components/header/Header"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //import { useFetchResults } from "./hook/useFetchResults";
 function App() {
@@ -9,8 +10,13 @@ function App() {
   //const { characters: items } = useFetchResults();
   return (
     <>
-      <Header />
-      <Characters />
+      <BrowserRouter>
+
+        <Header />
+        <Characters />
+
+      </BrowserRouter>
+
     </>
 
   )
--- project-rickandmorty/src/components/characters/Characters.jsx
+++ project-rickandmorty/src/components/characters/Characters.jsx
@@ -3,23 +3,49 @@ import Cards from '../cards/Cards';
 import Search from '../search/Search';
 import Pagination from '../pagination/Pagination';
 import FilterOptions from '../filterOptions/FilterOptions';
+import { useSearchParams } from 'react-router-dom';
 
 
 
 const Characters = () => {
+
+
     const urlBase = "https://rickandmortyapi.com/api/character";
-    const [pageNumber, setPageNumber] = useState(1);
-    const [status, setStatus] = useState("");
-    const [species, setSpecies] = useState("");
-    const [search, setSearch] = useState("");
-    const [gender, setGender] = useState("");
-    const [charactersToShow, setCharactersToShow] = useState();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const [pageNumber, setPageNumber] = useState(Number(searchParams.get('page')) || 1);
+    const [status, setStatus] = useState(searchParams.get('status') || "");
+    const [species, setSpecies] = useState(searchParams.get('species') || "");
+    const [search, setSearch] = useState(searchParams.get('search') || "");
+    const [gender, setGender] = useState(searchParams.get('gender') || "");
+    const [charactersToShow, setCharactersToShow] = useState(Number(searchParams.get('characterstoshow')) || 20);
 
     const query = `?page=${pageNumber}&name=${search}&status=${status}&species=${species}&gender=${gender}`;
+
     const [characters, setCharacters] = useState([]);
     const [info, setInfo] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(null);
+
+
+    useEffect(() => {
+        setPageNumber(Number(searchParams.get('page')) || 1);
+        setStatus(searchParams.get('status') || '');
+        setSpecies(searchParams.get('species') || '');
+        setSearch(searchParams.get('name') || '');
+        setGender(searchParams.get('gender') || '');
+    }, [searchParams]);
+
+    useEffect(() => {
+        setSearchParams({
+            page: `${pageNumber}`,
+            name: `${search}`,
+            status: `${status}`,
+            species: `${species}`,
+            gender: `${gender}`,
+            characterstoshow: `${charactersToShow}`
+        })
+    }, [pageNumber, search, status, species, gender])
 
 
     useEffect(() => {
